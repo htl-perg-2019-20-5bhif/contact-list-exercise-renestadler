755c4451cc120006980e4f2b8f2a5b51474d072a
renestadler <stadler.rene2000@gmail.com>
2019-10-02 11:05:47 +0200

    First & Final Version

---
+++ ContactList/Controllers/ContactsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContactList.Controllers
+{
+    [ApiController]
+    [Route("/contacts")]
+    public class ContactsController : ControllerBase
+    {
+
+        private static int ID = 0;
+        private static readonly List<Person> People = new List<Person>();
+
+        private readonly ILogger<ContactsController> _logger;
+
+        public ContactsController(ILogger<ContactsController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllPeople()
+        {
+            return Ok(People);
+        }
+
+        [HttpPost]
+        public virtual IActionResult AddPerson([FromBody]Person body)
+        {
+            if (body == null || body.id < 0 ||
+                body.email == null || body.email.Length == 0)
+            {
+                return BadRequest("Invalid input (e.g. required field missing or empty)");
+            }
+            IEnumerable<Person> deleteQuery = from person in People where person.id == body.id select person;
+            if (deleteQuery.Count() != 0)
+            {
+                return BadRequest("ID already choosen");
+
+            }
+            People.Add(body);
+            return Created("", body);
+        }
+
+        [HttpDelete]
+        [Route("/contacts/{personId}")]
+        public virtual IActionResult DeletePerson([FromRoute]int personId)
+        {
+            if (personId < 0)
+            {
+                return BadRequest("Invalid ID supplied");
+            }
+
+            IEnumerable<Person> deleteQuery = from person in People where person.id == personId select person;
+            if (deleteQuery.Count() == 0)
+            {
+                return NotFound("Person not found");
+            }
+            People.Remove(deleteQuery.Single());
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("/contacts/findByName")]
+        public virtual IActionResult FindPersonByName([FromQuery]string nameFilter)
+        {
+            if (nameFilter == null)
+            {
+                return BadRequest("Invalid or missing name");
+            }
+            IEnumerable<Person> deleteQuery = from person in People where (person.firstName != null && person.firstName.Equals(nameFilter)) || (person.lastName != null && person.lastName.Equals(nameFilter)) select person;
+            return Ok(deleteQuery);
+        }
+    }
+}
---
+++ ContactList/Person.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ContactList
+{
+    public class Person
+    {
+        public int id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+    }
+}
